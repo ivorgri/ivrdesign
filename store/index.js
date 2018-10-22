a90d7870dc6c1cf8a70514a0032d9d2ad35e48d5
ivorgri <ivorgrisel@gmail.com>
2018-10-22 20:48:19 +0200

    Updated Vuex store to contain pages and to retrieve homepage

--- store/index.js
+++ store/index.js
@@ -1,13 +1,19 @@
 const cmsApiUrl = 'https://cms.ivrdesign.nl/cockpit-master/api';
 const getSingletonUrl = `${cmsApiUrl}/singletons/get`;
+const getCollectionUrl = `${cmsApiUrl}/collections/get`;
 
 async function getSingleton(singleton, app) {
   return await app.$axios.$get(`${getSingletonUrl}/${singleton}?token=${process.env.CMS_API_TOKEN}`)
 }
 
+async function getCollection(collection, app) {
+  return await app.$axios.$get(`${getCollectionUrl}/${collection}?token=${process.env.CMS_API_TOKEN}`)
+}
+
 export const state = () => ({
   online: true,
   contactDetails: {},
+  pages: [],
 })
 
 export const getters = {
@@ -16,6 +22,9 @@ export const getters = {
   },
   getContactDetails: state => {
     return state.contactDetails;
+  },
+  getHomepage: state => {
+    return state.pages.find((page) => page.template === 'Homepage');
   },
 }
 
@@ -26,19 +35,24 @@ export const mutations = {
   SET_CONTACTDETAILS (state, contactDetails) {
     state.contactDetails = contactDetails;
   },
+  SET_PAGES (state, pages) {
+    state.pages = pages;
+  },
 }
 
 export const actions = {
   async nuxtServerInit({ commit },{ app }) {
-    let [ contactDetails ] = [{}];
+    let [ contactDetails, pages ] = [{},{}];
     try {
-      [ contactDetails ] = await Promise.all([
+      [ contactDetails, pages ] = await Promise.all([
         getSingleton('contactDetails', app),
+        getCollection('pages', app)
       ]);
     } catch (error) {
       console.log(error);
     }
 
     commit('SET_CONTACTDETAILS', contactDetails);
+    commit('SET_PAGES', pages.entries);
   }
 }
