93494fb3af5d05aa0cce4f88ca500dfc702782d3
ivorgri <ivorgrisel@gmail.com>
2018-10-23 12:29:32 +0200

    Added fontawesome package and created plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,6 +61,7 @@ module.exports = {
   plugins: [
     // https://github.com/nuxt/nuxt.js/issues/2367
     { src: '~/middleware/connectionStatus.js', ssr: false },
+    { src: '~/plugins/font-awesome'},
   ],
 
   /*
@@ -70,7 +71,6 @@ module.exports = {
     '@nuxtjs/pwa',
     '@nuxtjs/axios',
     '@nuxtjs/dotenv',
-    '@nuxtjs/font-awesome',
   ],
 
   /*
