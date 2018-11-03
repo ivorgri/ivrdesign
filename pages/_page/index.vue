31cef9f46f0db14c6afda5be927d069536516d84
ivorgri <ivorgrisel@gmail.com>
2018-11-03 19:44:14 +0100

    Removed container section

--- pages/_page/index.vue
+++ pages/_page/index.vue
@@ -1,64 +1,59 @@
 <template>
-  <section
-    :class="containerClass"
-    class="container"
-    data-cy="container">
-    <transition-group
-      name="fade"
-      mode="out-in">
-      <div
-        v-if="isOnline"
-        id="online-content"
-        key="online-content"
-        class="content"
-        data-cy="content"
-        v-html="onlineContent"/>
-      <div
-        v-else-if="!isOnline && !offlineContent"
-        id="online-offline-content"
-        key="online-offline-content"
-        class="content"
-        data-cy="content"
-        v-html="onlineContent"/>
-      <div
-        v-else-if="!isOnline && offlineContent"
-        id="offline-content"
-        key="offline-content"
-        class="content"
-        data-cy="content"
-        v-html="offlineContent"/>
-      <div
-        v-else
-        id="fallback-content"
-        key="fallback-content"
-        class="content"
-        data-cy="content"
-        v-html="onlineContent"/>
-      <hr key="splitter">
-      <div
-        v-if="$store.getters.getBlogPosts.length <= 0"
-        id="no-blog-posts"
-        key="no-blog-posts"
-        data-cy="no-blog-posts">
-        {{ page.listFallback }}
-      </div>
-      <div
-        v-else-if="blogPosts.length > 0"
-        id="blog-posts"
-        key="blog-posts"
-        data-cy="blog-posts">
-        <nuxt-link
-          v-for="blogPost in blogPosts"
-          :to="`/blog/${blogPost.slug_slug}`"
-          :key="blogPost.title"
-          class="blog-post-link">
-          <h2 class="blog-post-link-title">{{ blogPost.title }}</h2>
-          <span class="blog-post-link-create-date">{{ blogPost._created | formatDate }}</span>
-          <span class="blog-post-link-preview">{{ blogPost.preview }}</span>
-        </nuxt-link>
-      </div>
-    </transition-group>
-  </section>
+  <transition-group
+    name="fade"
+    mode="out-in">
+    <div
+      v-if="isOnline"
+      id="online-content"
+      key="online-content"
+      class="content"
+      data-cy="content"
+      v-html="onlineContent"/>
+    <div
+      v-else-if="!isOnline && !offlineContent"
+      id="online-offline-content"
+      key="online-offline-content"
+      class="content"
+      data-cy="content"
+      v-html="onlineContent"/>
+    <div
+      v-else-if="!isOnline && offlineContent"
+      id="offline-content"
+      key="offline-content"
+      class="content"
+      data-cy="content"
+      v-html="offlineContent"/>
+    <div
+      v-else
+      id="fallback-content"
+      key="fallback-content"
+      class="content"
+      data-cy="content"
+      v-html="onlineContent"/>
+    <hr key="splitter">
+    <div
+      v-if="$store.getters.getBlogPosts.length <= 0"
+      id="no-blog-posts"
+      key="no-blog-posts"
+      data-cy="no-blog-posts">
+      {{ page.listFallback }}
+    </div>
+    <div
+      v-else-if="blogPosts.length > 0"
+      id="blog-posts"
+      key="blog-posts"
+      data-cy="blog-posts">
+      <nuxt-link
+        v-for="blogPost in blogPosts"
+        :to="`/blog/${blogPost.slug_slug}`"
+        :key="blogPost.title"
+        class="blog-post-link">
+        <h2 class="blog-post-link-title">{{ blogPost.title }}</h2>
+        <span class="blog-post-link-create-date">{{ blogPost._created | formatDate }}</span>
+        <span class="blog-post-link-preview">{{ blogPost.preview }}</span>
+      </nuxt-link>
+    </div>
+  </transition-group>
 </template>
 
 <script>
@@ -66,9 +61,6 @@
     computed: {
       cleanSlug() {
         return this.$route.params.page;
-      },
-      containerClass() {
-        return `${this.cleanSlug}-container`;
       },
       page() {
         return this.$store.getters.getPageBySlug(this.cleanSlug);
