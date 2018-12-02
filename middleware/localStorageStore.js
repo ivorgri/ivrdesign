aa60ca6a3c626283a306529ba06c197572acd9e9
ivorgri <ivorgrisel@gmail.com>
2018-12-02 10:40:21 +0100

    If there is a store in LS, initialize it in the store

--- middleware/localStorageStore.js
+++ middleware/localStorageStore.js
@@ -1,5 +1,9 @@
 export default function ({ store }) {
   if (process.client) {
+    if(localStorage.getItem('store')) {
+      let savedStore = JSON.parse(localStorage.getItem('store'));
+      store.commit('INITIALIZE_STORE', savedStore);
+    }
     store.subscribe((mutation,state) => {
       localStorage.setItem('store', JSON.stringify(state));
     });
